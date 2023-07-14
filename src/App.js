dccee54c507f7b0489795e4262c5d21f57555312
MichaelWiss <michael.wiss@gmail.com>
2023-07-13 19:18:47 -0500

    Buttons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import Button from "./Button";
+
 function App() {
-    return <div>App</div>;
+    return <div>
+        <div>
+            <Button></Button>
+        </div>
+        <div>
+            <Button></Button>
+        </div>
+        <div>
+            <Button></Button>
+        </div>
+        <div>
+            <Button></Button>
+        </div>
+    </div>;
 }
 
 export default App;
---
+++ src/Button.js
@@ -0,0 +1,5 @@
+function Button () {
+    return <Button>Hi There</Button>
+}
+
+export default Button;
