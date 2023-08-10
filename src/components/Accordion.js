3a8d36ad48f7b9e500092ebd126d5c75682a4833
MichaelWiss <michael.wiss@gmail.com>
2023-08-10 17:45:53 -0500

    conditional rendering

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -6,13 +6,15 @@ function Accordion({ items }) {
     const renderedItems = items.map((item, index) => {
         const isExpanded = index === expandedIndex;
 
-        console.log(isExpanded);
+        const content = isExpanded && <div>{item.content}</div>
+
+        
 
 
         return (
             <div key={item.id}>
                 <div>{item.label}</div>
-                <div>{item.content}</div>
+                {content}
             </div>
         )
     });
