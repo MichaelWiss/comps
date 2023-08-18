c3670b2c7f3d27333cf2de48fe1360d3e609b202
MichaelWiss <michael.wiss@gmail.com>
2023-08-18 18:08:45 -0500

    setting state for dropdown

--- src/App.js
+++ src/App.js
@@ -1,3 +1,13 @@
-function App() {}
+import Dropdown from './components/Dropdrown';
+function App() {
+    const options = [
+        { label: 'Red', value: 'red' },
+        { label: 'Green', value: 'green' },
+        { label: 'Blue', value: 'blue' },
+    ]
+
+
+    return <Dropdown options={options} />;
+}
 
 export default App;
--- src/components/Dropdrown.js
+++ src/components/Dropdrown.js
@@ -1,5 +1,17 @@
-function() {
-    return <div>Dropdown...</div>;
+import { useState } from 'react';
+
+function Dropdown({ options }) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const renderdOptions = options.map((option) => {
+        return <div key={option.value}>
+            {option.label}
+        </div>
+    });
+    return <div>
+        <div>Select...</div>
+        <div>{renderdOptions}</div>
+    </div>;
 }
 
 export default Dropdown;
