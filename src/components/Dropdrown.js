bf908c40af5ff8f2938c84184dc6cd2c1b691dd9
MichaelWiss <michael.wiss@gmail.com>
2023-08-18 18:22:39 -0500

    toggle

--- src/components/Dropdrown.js
+++ src/components/Dropdrown.js
@@ -3,14 +3,18 @@ import { useState } from 'react';
 function Dropdown({ options }) {
     const [isOpen, setIsOpen] = useState(false);
 
+    const handleClick = () => {
+        setIsOpen(!isOpen);
+    };
+
     const renderdOptions = options.map((option) => {
         return <div key={option.value}>
             {option.label}
         </div>
     });
     return <div>
-        <div>Select...</div>
-        <div>{renderdOptions}</div>
+        <div onClick={handleClick}>Select...</div>
+        {isOpen && <div>{renderdOptions}</div>}
     </div>;
 }
 
