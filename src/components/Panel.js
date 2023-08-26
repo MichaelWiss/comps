cd0eed62b9f8e8e09ec56d4c6713d5b3674dd16f
MichaelWiss <michael.wiss@gmail.com>
2023-08-26 17:45:15 -0500

    classNames

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,10 +1,12 @@
 import classNames from 'classnames';
 
-function Panel({ children, classNames, ...rest }) {
+function Panel({ children, className, ...rest }) {
     const finalClassNames = classNames(
         'border rounded p-3 shadow bg-white w-full',
         className
-    )
+    );
+
+    return <div {...rest} className={finalClassNames}>{children}</div>
 }
 
 export default Panel;
