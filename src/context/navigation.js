0e6b85647825c7045dc2a3c9027a3ce9855e70c9
MichaelWiss <michael.wiss@gmail.com>
2023-08-31 16:33:01 -0500

    using context for nav

--- src/context/navigation.js
+++ src/context/navigation.js
@@ -2,7 +2,7 @@ import { createContext } from 'react';
 
 const NavigationContext = createContext();
 
-function NavigationProvider = ({children}) {
+function NavigationProvider ({children}) {
     return <NavigationContext.Provider value={{}}>
         {children}
     </NavigationContext.Provider>
