ec230d23c84596fa5d7b5c86c74b8e0f38bf033c
MichaelWiss <michael.wiss@gmail.com>
2023-09-01 17:56:58 -0500

    navigate

--- src/context/navigation.js
+++ src/context/navigation.js
@@ -22,12 +22,7 @@ function NavigationProvider ({children}) {
         setCurrentPath(to);
     };
 
-    return <NavigationContext.Provider value={{}}>
-        <div>
-            <button onClick={() => navigate('/accordion')}>Go to accordion</button>
-            <button onClick={() => navigate('/dropdown')}>Go to dropdown</button>
-        </div>
-        {currentPath}
+    return <NavigationContext.Provider value={{ currentPath, navigate}}>
         {children}
     </NavigationContext.Provider>
 }
