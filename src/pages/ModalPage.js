d88ea4c3035b841e1d21db61350598ad8c8b723f
MichaelWiss <michael.wiss@gmail.com>
2023-09-19 18:09:45 -0500

    modal state

--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -1,8 +1,17 @@
+import { useState } from 'react';
 import Modal from '../components/Modal';
+import Button from '../components/Button';
 
 function ModalPage() {
+    const [showModal, setShowModal] = useState(false);
+
+    const handleClick = () => {
+        setShowModal(true);
+    };
+
      return (
         <div>
+            <Button primay>Open Model</Button>
             <Modal />
         </div>
      )
