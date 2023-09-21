405ba07018534b72b2cb64b5067a1de4ff84d6e1
MichaelWiss <michael.wiss@gmail.com>
2023-09-21 18:04:00 -0500

    modal message

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,8 +4,8 @@ function Modal() {
     return ReactDOM.createPortal(
         <div>
             <div className="absolute inset-0 bg-gray-300 opacity-80"></div>
-            <div className="absolute inset-40 p-10 bg-white"></div>
-           Here's the MODAL!
+            <div className="absolute inset-40 p-10 bg-white">Here's the MODAL!</div>
+           
        </div>,
         document.querySelector('.modal-container')
     );
