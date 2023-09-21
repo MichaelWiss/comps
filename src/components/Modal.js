dcca0698b053a6f8a234a0b0bd11902e1c927655
MichaelWiss <michael.wiss@gmail.com>
2023-09-20 19:27:31 -0500

    portal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,10 +1,14 @@
+import * as ReactDOM from 'react-dom';
+
 function Modal() {
-    return <div>
-       <div className="absolute inset-0 bg-gray-300 opacity-80"></div>
-       <div className="absolute inset-40 p-10 bg-white">
+    return ReactDOM.createPortal(
+        <div>
+            <div className="absolute inset-0 bg-gray-300 opacity-80"></div>
+            <div className="absolute inset-40 p-10 bg-white"></div>
            Here's the MODAL!
-       </div>
-    </div>;
+       </div>,
+        document.querySelector('.modal-container')
+    );
 }
 
 export default Modal;
