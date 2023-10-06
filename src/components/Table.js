dc1f5cd38e5c081837641ece57eaccde9a1985db
MichaelWiss <michael.wiss@gmail.com>
2023-10-06 13:02:03 -0500

    renderedCells

--- src/components/Table.js
+++ src/components/Table.js
@@ -6,11 +6,9 @@ function Table({ data, config }) {
     const renderedRows = data.map((fruit) => {
         return (
             <tr className="border-b" key={fruit.name}>
-                <td className="p-3">{config[0].render(fruit)}</td>
-                <td className="p-3">
-                    {config[1].render(fruit)}
-                </td>
-                <td className="p-3">{config[2].render(fruit)}</td>
+                const renderedCells = config.map((column) => {
+
+                })
             </tr>
         );
     });
