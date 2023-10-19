cdf544ce3d04d2a481c5f90eed86d2ffb294ecc3
MichaelWiss <michael.wiss@gmail.com>
2023-10-19 14:01:19 -0500

    counter page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import DropdownPage from './pages/DropDownPage';
 import ButtonPage from './pages/ButtonPage';
 import ModalPage from './pages/ModalPage';
 import TablePage from './pages/TablePage';
+import CounterPage from './pages/CounterPage';
 
 function App() {
     return (
@@ -24,8 +25,11 @@ function App() {
                 <ModalPage />
             </Route>
             <Route path="/table">
-            <TablePage />
-        </Route>
+                <TablePage />
+            </Route>
+            <Route path="/counter">
+                <CounterPage initialCount={10} />
+            </Route>
         </div>
     </div>
     );
