857f3c4041a5b5d612a90a77fc29bc9f1137af8a
MichaelWiss <michael.wiss@gmail.com>
2023-11-29 17:35:27 -0600

    handleChange

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,11 +1,13 @@
 
 import { useState } from 'react';
 import Button from '../components/Button';
+import Panel from '../components/Panel';
 
 
 
 function CounterPage({ initialCount }) {
     const [count, setCount] = useState(initialCount);
+    const [value, setValueToAdd] = useState(0);
     
     const increment = () => {
         setCount(count + 1);
@@ -15,16 +17,34 @@ function CounterPage({ initialCount }) {
         setCount(count - 1);
     };
 
+    const handleChange = (event) => {
+        const value = parseInt(event.target.value);
+
+        console.log(typeof value);
+    };
+
     return (
-    <div>
-        <h1>Count is {count}</h1>
-        <Button onClick={increment}>
-            Increment
-        </Button>
-        <Button onClick={decrement}>
-            Decrement
-        </Button>
-    </div>
+    <Panel className="m-3">
+        <h1 className="text-lg">Count is {count}</h1>
+        <div className="flex flex-row">
+            <Button onClick={increment}>
+                Increment
+            </Button>
+            <Button onClick={decrement}>
+                Decrement
+            </Button>
+        </div>
+
+        <form action="">
+            <label htmlFor="">Add a lot!</label>
+            <input 
+                value={valueToAdd}
+                onChange={handleChange}
+                type="number" 
+                className='p-1 m-3 bg-gray-50 border border-gray-300'/>
+            <Button>Add It!</Button>
+        </form>
+    </Panel>
     )
 }
 
