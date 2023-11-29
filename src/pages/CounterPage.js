a1d2fc68c47e69bf3441310997aecc045b36f816
MichaelWiss <michael.wiss@gmail.com>
2023-11-29 16:46:19 -0600

    clickHandler

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -5,14 +5,24 @@ import Button from '../components/Button';
 
 
 function CounterPage({ initialCount }) {
-    const [count, setCount] = useState();
+    const [count, setCount] = useState(initialCount);
     
+    const increment = () => {
+        setCount(count + 1);
+    };
+
+    const decrement = () => {
+        setCount(count - 1);
+    };
 
     return (
     <div>
         <h1>Count is {count}</h1>
         <Button onClick={increment}>
             Increment
+        </Button>
+        <Button onClick={decrement}>
+            Decrement
         </Button>
     </div>
     )
