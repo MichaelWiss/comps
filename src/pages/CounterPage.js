807491592c4edf1cc02589b37e67bbadaa67a1a7
MichaelWiss <michael.wiss@gmail.com>
2023-12-07 18:21:56 -0600

    constants

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -3,23 +3,33 @@ import { useReducer } from 'react';
 import Button from '../components/Button';
 import Panel from '../components/Panel';
 
+const INCREMENT_COUNT = 'increment';
+const SET_VALUE_TO_ADD = 'set-value-to-add';
+
 
 const reducer = (state, action) => {
-    if (action.type === 'increment') {
-        return {
-            ...state,
-            count: state.count + 1
-        };
-    }
+    switch (action.type) {
+        case INCREMENT_COUNT:
 
-    if (action.type === 'change-value-to-add') {
-        return {
-            ...state,
-            valueToAdd: action.payload,
-        };
-    }
+        case SET_VALUE_TO_ADD:
 
-    return state;
+        default:
+    }
+    // if (action.type === 'INCREMENT_COUNT') {
+    //     return {
+    //         ...state,
+    //         count: state.count + 1
+    //     };
+    // }
+
+    // if (action.type === 'SET_VALUE_TO_ADD') {
+    //     return {
+    //         ...state,
+    //         valueToAdd: action.payload,
+    //     };
+    // }
+
+    // return state;
 };
 
 
@@ -33,10 +43,10 @@ function CounterPage({ initialCount }) {
         valueToAdd: 0,
     });
     console.log(state);
-    
+
     const increment = () => {
         dispatch({
-            type: 'increment'
+            type: 'INCREMENT_COUNT'
         });
     };
 
@@ -48,7 +58,7 @@ function CounterPage({ initialCount }) {
         const value = parseInt(event.target.value) || 0;
 
         dispatch({
-            type: 'change-value-to-add',
+            type: 'SET_VALUE_TO_ADD',
             payload: value
         });
     };
